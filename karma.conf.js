efe181a4e7893d9b34b79fd7e3d6dc643247353b
Assaf Moldavsky <kur8or@moldavsky.net>
2016-09-12 08:43:39 -0400

    fixed the dependencies

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
         // list of files / patterns to load in the browser
         files: [
             'test/angular-1.5.8/angular.min.js',
-            'test/angular-1.5.8/angular-mocks.min.js',
+            'test/angular-1.5.8/angular-mocks.js',
             'src/angular-mocks-async.js',
             'test/angular-mocks-async-test.js'
         ],
--- test/angular-mocks-async-test.js
+++ test/angular-mocks-async-test.js
@@ -1,13 +1,24 @@
 //-------------------------- Prep:
 
 //-------------------------- Tests:
+(function( ng ) {
+    describe( 'ngMockAsync Test', function() {
 
-describe( 'ngMockAsync Test', function() {
+        it( 'test module init', function() {
 
-    it( 'test module init', function() {
+            var app = ng.module( 'mockApp', [ 'ngMockE2EAsync' ] );
 
-        var app = ng.module( 'mockApp', [ 'ngMockE2EAsync' ] );
+        });
 
     });
 
-});
+    describe( 'ngMockAsync Test 2', function() {
+
+        it( 'test module init2', function() {
+
+            var app = ng.module( 'mockApp', [ 'ngMockE2EAsync' ] );
+
+        });
+
+    });
+})( angular );
