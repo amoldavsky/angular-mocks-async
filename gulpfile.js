4375cf9732d98519113d709a15518b52c56d3677
amoldavsky <assaf.moladvsky@gmail.com>
2017-01-29 11:40:48 -0500

    fixed the compile task

--- gulpfile.js
+++ gulpfile.js
@@ -7,49 +7,67 @@
     gulp.task( "compile", function () {
         const compressor = require( "node-minify" );
 
-        /// copy sources
-        try {
+        return new Promise( function( resolve, reject ) {
 
-            const srcFile = 'src/angular-mocks-async.js';
-            const destFile = 'dist/angular-mocks-async.js';
 
-            fs.createReadStream(
-                srcFile
-            ).pipe( fs.createWriteStream(
-                destFile
-            ));
+            let copyPromise = new Promise( function( resolve, reject ) {
 
-            console.log( `processed: ${destFile}` );
+                /// copy sources
+                try {
 
-        } catch( e ) {
-            console.error( e );
-            throw e;
-        }
+                    const srcFile = 'src/angular-mocks-async.js';
+                    const destFile = 'dist/angular-mocks-async.js';
 
-        // minify all distributions
-        try {
+                    fs.createReadStream(
+                        srcFile
+                    ).pipe(fs.createWriteStream(
+                        destFile
+                    ).on('finish', function () {
+                        resolve( destFile );
+                    }));
 
-            const srcFile = 'dist/angular-mocks-async.js';
-            const destFile = 'dist/angular-mocks-async.min.js';
+                    console.log(`processed: ${destFile}`);
 
-            compressor.minify({
-                compressor: 'yui-js',
-                input: srcFile,
-                output: destFile,
-                sync: true,
-                callback: function( error ) {
-                    if( error ) {
-                        console.error( error );
-                        throw error;
-                    }
-
-                    console.log(`minified: ${destFile}`);
+                } catch (e) {
+                    console.error(e);
+                    throw e;
                 }
             });
-        } catch( e ) {
-            console.error( e );
-            throw e;
-        }
+
+            // minify all distributions
+            return copyPromise.then( function( compiledFile ) {
+                try {
+
+                    // generate dest file
+                    const destFile = (function( srcFile ) {
+                        let split = srcFile.split( "." );
+                        split.splice( split.length - 1, 0, "min" );
+                        return split.join( "." );
+                    })( compiledFile );
+
+                    compressor.minify({
+                        compressor: 'gcc',
+                        input: compiledFile,
+                        output: destFile,
+                        sync: true,
+                        options: {},
+                        callback: function( error ) {
+                            if( error ) {
+                                console.error( error );
+                                throw error;
+                            }
+
+                            console.log(`minified: ${destFile}`);
+
+                            resolve();
+                        }
+                    });
+                } catch( e ) {
+                    console.error( e );
+                    throw e;
+                }
+            });
+        });
     });
 
     gulp.task( "test-unminified", function () {
