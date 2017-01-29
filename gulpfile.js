501cb20451b24377cf60340ff31d504709dc6484
amoldavsky <assaf.moladvsky@gmail.com>
2017-01-29 11:17:47 -0500

    updated gulp tests

--- gulpfile.js
+++ gulpfile.js
@@ -4,69 +4,7 @@
     const karma = require( "gulp-karma-runner" );
     const fs = require('fs');
 
-    gulp.task("test-unminified", function () {
-        gulp.src([
-            'test/angular-1.5.8/angular.js',
-            'test/angular-1.5.8/angular-mocks.js',
-            'src/angular-mocks-async.js',
-            'test/angular-mocks-async-internal-tests.js',
-            'test/angular-mocks-async-test.js'
-        ], {"read": false}).pipe(
-            karma.server({
-                singleRun: true,
-                autoWatch: true,
-                concurrency: Infinity,
-                port: 9876,
-
-                frameworks: [ "mocha", "chai" ],
-                browsers: [ "Chrome" ],
-                basePath: "",
-                exclude: [],
-                preprocessors: {
-                    'src/**/*.js': ['coverage']
-                },
-                reporters: [ 'spec', 'coverage' ],
-                coverageReporter: {
-                    type : 'lcov',
-                    dir : 'coverage/'
-                },
-                colors: true
-            })
-        );
-    });
-
-    gulp.task("test-minified", function () {
-        gulp.src([
-            'test/angular-1.5.8/angular.js',
-            'test/angular-1.5.8/angular-mocks.js',
-            'src/angular-mocks-async.js',
-            'test/angular-mocks-async-internal-tests.js',
-            'test/angular-mocks-async-test.js'
-        ], {"read": false}).pipe(
-            karma.server({
-                singleRun: true,
-                autoWatch: true,
-                concurrency: Infinity,
-                port: 9876,
-
-                frameworks: [ "mocha", "chai" ],
-                browsers: [ "Chrome" ],
-                basePath: "",
-                exclude: [],
-                preprocessors: {
-                    'src/**/*.js': ['coverage']
-                },
-                reporters: [ 'spec', 'coverage' ],
-                coverageReporter: {
-                    type : 'lcov',
-                    dir : 'coverage/'
-                },
-                colors: true
-            })
-        );
-    });
-
-    gulp.task("compile", function () {
+    gulp.task( "compile", function () {
         const compressor = require( "node-minify" );
 
         /// copy sources
@@ -112,6 +50,68 @@
             console.error( e );
             throw e;
         }
+    });
+
+    gulp.task( "test-unminified", function () {
+        gulp.src([
+            'test/angular-1.5.8/angular.js',
+            'test/angular-1.5.8/angular-mocks.js',
+            'dist/angular-mocks-async.js',
+            'test/angular-mocks-async-internal-tests.js',
+            'test/angular-mocks-async-test.js'
+        ], {"read": false}).pipe(
+            karma.server({
+                singleRun: true,
+                autoWatch: true,
+                concurrency: Infinity,
+                port: 9876,
+
+                frameworks: [ "mocha", "chai" ],
+                browsers: [ "Chrome" ],
+                basePath: "",
+                exclude: [],
+                preprocessors: {
+                    'src/**/*.js': ['coverage']
+                },
+                reporters: [ 'spec', 'coverage' ],
+                coverageReporter: {
+                    type : 'lcov',
+                    dir : 'coverage/'
+                },
+                colors: true
+            })
+        );
+    });
+
+    gulp.task( "test-minified", function () {
+        gulp.src([
+            'test/angular-1.5.8/angular.js',
+            'test/angular-1.5.8/angular-mocks.js',
+            'dist/angular-mocks-async.min.js',
+            'test/angular-mocks-async-internal-tests.js',
+            'test/angular-mocks-async-test.js'
+        ], {"read": false}).pipe(
+            karma.server({
+                singleRun: true,
+                autoWatch: true,
+                concurrency: Infinity,
+                port: 9876,
+
+                frameworks: [ "mocha", "chai" ],
+                browsers: [ "Chrome" ],
+                basePath: "",
+                exclude: [],
+                preprocessors: {
+                    'src/**/*.js': ['coverage']
+                },
+                reporters: [ 'spec', 'coverage' ],
+                coverageReporter: {
+                    type : 'lcov',
+                    dir : 'coverage/'
+                },
+                colors: true
+            })
+        );
     });
 
 }( require ));
