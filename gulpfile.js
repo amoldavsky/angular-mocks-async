007b945d2051a3faac130144b6a64f0770f6330c
amoldavsky <assaf.moladvsky@gmail.com>
2017-01-29 12:20:11 -0500

    added anguar 1.3.0 for tests

--- gulpfile.js
+++ gulpfile.js
@@ -130,4 +130,35 @@
         );
     });
 
+    gulp.task( "test-with-angular-1.3.0", function () {
+        gulp.src([
+            'test/angular-1.3.0/angular.js',
+            'test/angular-1.3.0/angular-mocks.js',
+            'dist/angular-mocks-async.min.js',
+            'test/angular-mocks-async-internal-tests.js',
+            'test/angular-mocks-async-test.js'
+        ], {"read": false}).pipe(
+            karma.server({
+                singleRun: true,
+                autoWatch: true,
+                concurrency: Infinity,
+                port: 9876,
+
+                frameworks: [ "mocha", "chai" ],
+                browsers: [ "Chrome" ],
+                basePath: "",
+                exclude: [],
+                preprocessors: {
+                    'src/**/*.js': ['coverage']
+                },
+                reporters: [ 'spec', 'coverage' ],
+                coverageReporter: {
+                    type : 'lcov',
+                    dir : 'coverage/'
+                },
+                colors: true
+            })
+        );
+    });
+
 }( require ));
